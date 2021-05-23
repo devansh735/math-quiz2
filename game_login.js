23f6e6712c35feef21e38c9076e646ea159c3383
devansh735 <74194267+devansh735@users.noreply.github.com>
2021-05-23 21:07:01 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,16 @@
+function add_user(){
+    player1_name= document.getElementById("player1_name").value;
+    player2_name=document.getElementById("player2_name").value;
+
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+    window.location="game_page.html";
+}
+
+function update_name(){
+    player_add1=document.getElementById("player_add1");
+    player_add2=document.getElementById("player_add2");
+
+    localStorage.getItem("player1_name",player1_name);
+    localStorage.getItem("player2_name",player2_name);
+}
